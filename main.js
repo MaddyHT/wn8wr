10fab1c4200fc9f29575b570431161f6c762b3ec
MaddyHT <MaddyHT@yandex.ru>
2022-01-04 11:19:38 +0500

    master

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
  * The address of the battle results websocket server.
  * @type {string}
  */
-const SERVER_ADDRESS = 'wss://localhost:15455';
+const SERVER_ADDRESS = 'ws://localhost:15455';
 
 
 // ============================================================================
